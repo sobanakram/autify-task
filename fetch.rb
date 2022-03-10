67fdfafb35cccf8cdd4244bf7e303a6c8c2ad0dd
Muhammad Soban <soban.se@gmail.com>
2022-03-10 16:23:24 +0500

    code refactored

--- fetch.rb
+++ fetch.rb
@@ -11,6 +11,32 @@ program :name, 'fetch'
 program :version, '0.0.1'
 program :description, 'Download Webpages'
 
+def download_website(website)
+  uri = URI.parse(website)
+  response = Net::HTTP.get_response uri
+  file_name = "#{uri.hostname}.html"
+  File.open(file_name, 'w') do |f|
+    f.write(response.body)
+  end
+  say "Webpage #{website} downloaded in #{file_name}"
+  Website.record(website, uri.hostname, response.body)
+rescue Exception => e
+  puts "Failed to download webpage: #{website}"
+  puts "Error: #{e.message}"
+end
+
+def show_metadata(website)
+  website = Website.find_by(name: website)
+  if website.present?
+    puts "site: #{website.host}"
+    puts "num_links: #{website.links_count}"
+    puts "images: #{website.images_count}"
+    puts "last_fetch: #{website.last_fetched_at}"
+  else
+    puts "Website #{arg} haven't fetched yet"
+  end
+end
+
 command :download do |c|
   c.syntax = 'fetch'
   c.description = 'Download Webpages'
@@ -18,28 +44,10 @@ command :download do |c|
   c.action do |args, options|
     options.default :metadata => nil
     if options.metadata.present?
-      website = Website.find_by(name: options.metadata)
-      if website.present?
-        puts "site: #{website.host}"
-        puts "num_links: #{website.links_count}"
-        puts "images: #{website.images_count}"
-        puts "last_fetch: #{website.last_fetched_at}"
-      else
-        puts "Website #{arg} haven't fetched yet"
-      end
+      show_metadata(options.metadata)
     else
       args.each do |arg|
-        uri = URI.parse(arg)
-        response = Net::HTTP.get_response uri
-        file_name = "#{uri.hostname}.html"
-        File.open(file_name, 'w') do |f|
-          f.write(response.body)
-        end
-        say "Webpage #{arg} downloaded in #{file_name}"
-        Website.record(arg, uri.hostname, response.body)
-      rescue Exception => e
-        puts "Failed to download webpage: #{arg}"
-        puts "Error: #{e.message}"
+        download_website(arg)
       end
     end
   end
